07bbed2dc810f5406e7e8c56020c1dad11378133
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-01-27 20:55:26 +0000

    remove comment

--- express_server.js
+++ express_server.js
@@ -3,22 +3,15 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const bodyParser = require('body-parser'); // npm install body-parser
-// const cookieParser = require('cookie-parser'); // npm install cookie-parser
-// cookie-parser replace to cookie-session
 const cookieSession = require('cookie-session'); // npm install cookie-session
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.urlencoded({ extended: false })); // this is a req.body from each page form value
 const bcrypt = require('bcryptjs'); // security for password
 const { findUserByEmail } = require('./helpers.js'); // Mocha and Chai test
-// const { getUserByEmail, accessCheck } = require('./helpers.js'); // Mocha and Chai test
 
 app.set('view engine', 'ejs'); // npm install ejs
 //
 //  MIDDLEWARE
 //
 app.use(bodyParser.urlencoded({ extended: false })); // this is a req.body from each page form value
-// app.use(cookieParser()); // set up cookie-parser
-// cookie-parser replace to cookie-session
 app.set('trust proxy', 1); // trust first proxy
 
 app.use(
@@ -32,11 +25,6 @@ app.use(
 ); // set up cookie-session
 
 // My URLs DB
-// const urlDatabase = {
-//   b2xVn2: 'http://www.lighthouselabs.ca',
-//   '9sm5xK': 'http://www.google.com',
-// };
-//
 // URLs Belong to Users
 const urlDatabase = {
   b6UTxQ: {
@@ -68,17 +56,6 @@ const users = {
   },
 };
 
-// new user can't use same email in our current users
-// const findUserByEmail = (email) => {
-// Refactor Helper Functions as findUserByEmail() in helper.js
-//   for (const userId in users) {
-//     const user = users[userId];
-//     if (user.email === email) {
-//       return user;
-//     }
-//   }
-//   return null;
-// };
 // global scope
 function generateRandomString() {
   var random = Math.random().toString(36).slice(7);
@@ -90,13 +67,6 @@ function generateRandomString() {
 // GET /
 //
 app.get('/', (req, res) => {
-  // test cookie-session start
-  // req.session.user_id = (req.session.user_id || 0) + 1;
-
-  // Write response
-  // res.end(req.session.user_id + ' user_id');
-  // test cookie-session end
-  // res.send('Hello!');
   let cookieUser = req.session['user_id'];
   if (!cookieUser) {
     res.redirect(`/login`);
@@ -118,37 +88,18 @@ app.get('/users.json', (req, res) => {
   res.json(users);
 });
 
-// app.get('/hello', (req, res) => {
-//   res.send('<html><body>Hello <b>World</b></body></html>\n');
-// });
-// app.get('/hello', (req, res) => {
-//   const templateVars = { greeting: 'Hello World!' };
-//   res.render('hello_world', templateVars);
-// });
-
 //
 // GET /urls
 //
 // Cookies in Express
 app.get('/urls', (req, res) => {
   let cookieUser = req.session['user_id'];
-  // console.log('GET ID: ', cookieUser);
-  // console.log('GET email: ', users[cookieUser];
   const templateVars = {
     user_id: cookieUser,
-    // email: users[cookieUser].email, // error
     user: users[cookieUser],
-    // shortURL: req.params.shortURL,
-    // req.params.shortURL is app.get('/login'
-    // longURL: urlDatabase[req.params.shortURL],
     urls: urlDatabase,
   };
-  // const templateVars = {
-  //   user_id: req.session['user_id'],
-  //   urls: urlDatabase,
-  // };
   res.render('urls_index', templateVars);
-  // res.render is views/urls_index.ejs page template
 });
 
 //
@@ -166,13 +117,10 @@ app.get('/urls/new', (req, res) => {
     res.redirect('/login');
   }
   const templateVars = {
-    // user_id: cookieUser,
-    // email: users[cookieUser].email, // error
     user: users[cookieUser],
     urls: urlDatabase,
   };
   res.render('urls_new', templateVars);
-  // res.render is views/urls_new.ejs page template
 });
 
 //
@@ -182,11 +130,8 @@ app.get('/urls/new', (req, res) => {
 // https://expressjs.com/en/guide/routing.html#route-parameters  Object parameter set up
 app.get('/urls/:shortURL', (req, res) => {
   let cookieUser = req.session['user_id'];
-  // console.log('GET shortURLID: ', cookieUser);
-  // console.log('GET shortURLemail: ', users[cookieUser]);
   console.log('GET shortURLshortURL: ', req.params.shortURL);
   console.log('GET shortURLlongURL: ', urlDatabase[req.params.shortURL]);
-  // shortURL is like a call back function(shortURL){ shortURL()}
   if (!cookieUser) {
     res.redirect(`/login`);
   }
@@ -198,15 +143,11 @@ app.get('/urls/:shortURL', (req, res) => {
   }
   const templateVars = {
     shortURL: req.params.shortURL,
-    // req.params.shortURL is :shortURL from app.get('/urls/:shortURL'
     longURL: urlDatabase[req.params.shortURL].longURL,
-    /* What goes here? */
     user_id: cookieUser,
-    // email: users[cookieUser].email, // error
     user: users[cookieUser],
   };
   res.render('urls_show', templateVars);
-  // res.render is views/urls_show.ejs page template
 });
 //
 // DELITE
@@ -223,9 +164,6 @@ app.post('/urls/:shortURL/delete', (req, res) => {
     return res.status(400).send('This URL can not delete');
   }
   delete urlDatabase[shortURL];
-  // let shortURLDelete = req.params.shortURL;
-  // delete urlDatabase[shortURLDelete];
-  // update for requirement
   res.redirect(`/urls`);
 });
 
@@ -244,9 +182,7 @@ app.get('/u/:shortURL', (req, res) => {
   if (!shortDatabase.includes(shortURL)) {
     return res.status(400).send('This URL does not exist longURL');
   }
-  // const longURL = ...
   const longURL = urlDatabase[req.params.shortURL].longURL;
-  // req.params.shortURL is app.get('/u/:shortURL'
   res.redirect(longURL);
 });
 
@@ -255,8 +191,6 @@ app.get('/u/:shortURL', (req, res) => {
 //
 app.post('/urls', (req, res) => {
   let cookieUser = req.session['user_id'];
-  // console.log(req.body); // Log the POST request body to the console
-  // res.send('Ok'); // Respond with 'Ok' (we will replace this)
   let shortU = generateRandomString();
   urlDatabase[shortU] = {
     longURL: req.body.longURL,
@@ -264,7 +198,6 @@ app.post('/urls', (req, res) => {
   };
   console.log('POST urls', urlDatabase);
   console.log('POST urls', urlDatabase[shortU]);
-  // longURL is urls_new.ejs from input form name="longURL"
   res.redirect(`/urls/${shortU}`);
 });
 
@@ -274,12 +207,9 @@ app.post('/urls', (req, res) => {
 // EDIT
 app.post('/urls/:shortURL', (req, res) => {
   let shortURL = req.params.shortURL;
-  // req.params.id is app.post('/urls/:id'
   console.log('after newURL', req.body);
   console.log('after newLongURL', req.body.newLongURL);
   let newLongURL = req.body.newLongURL;
-  // urlDatabase[shortURL] = newLongURL;
-  // newLongURL is urls_show.ejs from input form name="newLongURL"
   if (newLongURL) {
     urlDatabase[shortURL].longURL = newLongURL;
     res.redirect('/urls');
@@ -295,13 +225,10 @@ app.get('/login', (req, res) => {
   let cookieUser = req.session['user_id'];
   const templateVars = {
     shortURL: req.params.shortURL,
-    // req.params.shortURL is app.get('/login'
     longURL: urlDatabase[req.params.shortURL],
-    // user_id: req.session['user_id'],
     user: users[cookieUser],
   };
   res.render('login', templateVars);
-  // res.render is views/login.ejs page template
 });
 
 //
@@ -311,13 +238,10 @@ app.get('/register', (req, res) => {
   let cookieUser = req.session['user_id'];
   const templateVars = {
     shortURL: req.params.shortURL,
-    // req.params.shortURL is app.get('/register'
     longURL: urlDatabase[req.params.shortURL],
-    // user_id: req.session['user_id'],
     user: users[cookieUser],
   };
   res.render('register', templateVars);
-  // res.render is views/register.ejs page template
 });
 
 //
@@ -325,27 +249,16 @@ app.get('/register', (req, res) => {
 //
 app.post('/login', (req, res) => {
   let cookieUser = req.session['user_id'];
-  // let newUserName = req.body.username;
-  // console.log(req.session.username);
   let loginEmail = req.body.email;
   let loginPassword = req.body.password;
-  // let hashedPassword = bcrypt.hashSync(loginPassword, 10);
-  //
   if (!loginEmail || !loginPassword) {
     return res.status(400).send('email and password cannot be blank');
   }
   const user = findUserByEmail(loginEmail, users);
   console.log('check users DB', user);
   if (!user) {
-    // findUserByEmail() is null
     return res.status(400).send('a user with that email does not exist');
   }
-  // if (user.password !== loginPassword) {
-
-  // console.log(
-  //   'POST loginPassword: ',
-  //   bcrypt.compareSync(loginPassword, user.password)
-  // );
   console.log('POST loginUserPassword: ', user.password);
   if (!bcrypt.compareSync(loginPassword, user.password)) {
     // password check bcrypt.compareSync() // true password match, false password not match
@@ -353,21 +266,8 @@ app.post('/login', (req, res) => {
   }
   let loginId = user.id;
   console.log('check login ID', loginId);
-  // res.cookie('user_id', loginId);
   req.session['user_id'] = loginId; // storing the user id value with cookie session
-  // set server username as a cookie
-  // const templateVars = {
-  //   user_id: newUserName,
-  //   urls: urlDatabase,
-  // };
-  const templateVars = {
-    // user_id: loginId,
-    user: users[cookieUser],
-    email: loginEmail,
-    urls: urlDatabase,
-  };
   res.redirect('/urls');
-  // res.render('login', templateVars);
 });
 
 //
@@ -389,7 +289,6 @@ app.post('/register', (req, res) => {
   console.log('check DB', users);
   console.log('check email DB', user);
   if (user) {
-    // findUserByEmail() is user
     return res.status(400).send('a user with that email already exists');
   }
   users[registerId] = {
@@ -399,34 +298,16 @@ app.post('/register', (req, res) => {
     password: hashedPassword,
   };
   console.log('Add new user in DB', users);
-  const templateVars = {
-    // user_id: registerId,
-    user: users[cookieUser],
-    email: registerEmail,
-    urls: urlDatabase,
-  };
-  // res.cookie('user_id', registerId);
   req.session['user_id'] = registerId; // storing the user id value with cookie session
   // set server username as a cookie
   res.redirect('/urls');
-  // ERROR res.redirect can't has 2nd value of templateVars
-  // res.render('urls_index', templateVars);
 });
 
 //
 // POST /logout
 //
 app.post('/logout', (req, res) => {
-  // res.clearCookie('user_id');
   req.session['user_id'] = null;
-  // there is a cookie session in your Browser Developer tool of Network, But it's OK.
-  // Cos when u login change session, then logout change it too.
-  // app.use(
-  //   cookieSession({
-  //     name: 'session',
-  //     keys: [
-  //       '40767e34-50b6-489e-81dd-d3883d695663', // Maybe this uuid after logout session as random key
-  //       'f62a8200-3854-4f51-81ee-a27ce4d2c01f', // Maybe this uuid after logout session as random key
   res.redirect('/urls');
 });
 
